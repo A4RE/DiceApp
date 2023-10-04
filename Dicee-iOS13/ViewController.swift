a02109125a538f2ef18bda355c9c8d6dc1e7d6a2
Andrey Kovalenko <aakoval01@gmail.com>
2023-10-04 23:29:22 +0300

    -- Add random value generator

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - PROPERTIES
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var diceArray: [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // - WHEN VIEW LOAD - CHANGE FIRST IMAGE TO SIX
+        diceImageViewOne.image = UIImage(named: "DiceSix")
+        diceImageViewTwo.image = UIImage(named: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // We generate random numbers in range 0 to 6
+        var firstRandomNumber = Int.random(in: 0..<diceArray.count)
+        var secondRandomNumber = Int.random(in: 0..<diceArray.count)
+        // We take values from array by indicies we randomly generate
+        diceImageViewOne.image = UIImage(named: "\(diceArray[firstRandomNumber])")
+        diceImageViewTwo.image = UIImage(named: "\(diceArray[secondRandomNumber])")
+    }
+    
 }
 
